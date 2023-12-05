5065290abdc47203fffb746a69b468dd1ab0d75e
nooters <thomasschmidt.che.wy@gmail.com>
2023-12-05 04:43:13 -0700

    Added day 2, restructured project folder

---
+++ Day1/day1.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+
+internal class Day1
+{
+    private static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("d1input.txt");
+
+        int first = -2;
+        int last = 0;
+        int total = 0;
+
+        foreach (string s in lines)
+        {
+            first = -2;
+            last = 0;
+            foreach (char c in s)
+            {
+                if (c == '1' ||
+                c == '2' ||
+                c == '3' ||
+                c == '4' ||
+                c == '5' ||
+                c == '6' ||
+                c == '7' ||
+                c == '8' ||
+                c == '9')
+                {
+                    if (first == -2)
+                    {
+                        first = int.Parse(c.ToString());
+                    }
+                    last = int.Parse(c.ToString());
+                }
+            }
+            total += 10 * first + last;
+        }
+        Console.WriteLine(total);
+
+        //Part 2
+        total = 0;
+        int index = 0;
+        int word = 0;
+        string sub;
+        foreach (string s in lines)
+        {
+            first = -2;
+            last = 0;
+            index = 0;
+            word = 0;
+            foreach (char c in s)
+            {
+                //Instead of stapling on a word check to the previous part we can just check both words and numbers at the same time
+                sub = s.Substring(index);
+                if (sub.StartsWith("one") || sub.StartsWith("1")) { word = 1; }
+                else if (sub.StartsWith("two") || sub.StartsWith("2")) { word = 2; }
+                else if (sub.StartsWith("three") || sub.StartsWith("3")) { word = 3; }
+                else if (sub.StartsWith("four") || sub.StartsWith("4")) { word = 4; }
+                else if (sub.StartsWith("five") || sub.StartsWith("5")) { word = 5; }
+                else if (sub.StartsWith("six") || sub.StartsWith("6")) { word = 6; }
+                else if (sub.StartsWith("seven") || sub.StartsWith("7")) { word = 7; }
+                else if (sub.StartsWith("eight") || sub.StartsWith("8")) { word = 8; }
+                else if (sub.StartsWith("nine") || sub.StartsWith("9")) { word = 9; }
+
+                if (word != 0)
+                {
+                    if (first == -2)
+                    {
+                        first = word;
+                    }
+                    last = word;
+                }
+                word = 0;
+                index++;
+            }
+            total += 10 * first + last;
+        }
+        Console.WriteLine(total);
+    }
+}
---
+++ Day2/day2.cs
@@ -0,0 +1,106 @@
+using System;
+using System.IO;
+using System.Xml;
+
+internal class Day2
+{
+    private static void Main(string[] args)
+    {
+        //colors = red, green, blue
+        int[] power = { 0, 0, 0, 0 };
+        string sub;
+        string[] lines = File.ReadAllLines("d2input.txt");
+        int game = 0, score = 0, color = 0;
+        bool poss;
+
+        foreach (string s in lines)
+        {
+            sub = s;
+            game = int.Parse(sub.Substring(5, sub.IndexOf(':') - 5));
+            sub = sub.Substring(6 + game.ToString().Length);
+            Console.WriteLine(sub);
+            while(sub != "")
+            {
+                sub = sub.Trim();
+                //Parse number
+                color = int.Parse(sub.Substring(0, sub.IndexOf(' ')));
+                sub = sub.Substring(color.ToString().Length + 1);
+    
+                //Determine color, determine if the pull was possible
+                if(sub.StartsWith("red"))
+                {
+                    if(color > 12)
+                    {
+                        //impossible combo
+                        poss = false;
+                    }
+                    if(color > power[0])
+                    {
+                        power[0] = color;
+                    }
+                    sub = sub.Substring(3);
+                }
+                else if(sub.StartsWith("green"))
+                {
+                    if(color > 13)
+                    {
+                        //impossible combo
+                        poss = false;
+                    }
+                    if(color > power[1])
+                    {
+                        power[1] = color;
+                    }
+                    sub = sub.Substring(5);
+                }
+                else if(sub.StartsWith("blue"))
+                {
+                    if(color > 14)
+                    {
+                        //impossible combo
+                        poss = false;
+                    }
+                    if(color > power[2])
+                    {
+                        power[2] = color;
+                    }
+                    sub = sub.Substring(4);
+                }
+                else
+                {
+                    //Something broke!
+                    Console.WriteLine("Something Broke! Fix it.");
+                    break;
+                }
+                    
+                if(sub.StartsWith(',') || sub.StartsWith(';'))
+                {
+                    sub = sub.Substring(1);
+                }
+                
+            }
+            if(sub == "")
+            {
+                score += game;
+                Console.WriteLine("Game {0} possible", game);
+                Console.WriteLine("Score is now {0}", score);
+            }
+            else
+            {
+                Console.WriteLine("Game {0} is impossible", game);
+            }
+            power[3] += power[0] * power[1] * power[2];
+            Console.WriteLine("Power values: Red {0}, Green {1}, Blue {2}", power[0], power[1], power[2]);
+            Console.WriteLine("Power is now at {0}", power[3]);
+            power[0] = 0;
+            power[1] = 0;
+            power[2] = 0;
+
+            
+            
+        }
+
+
+
+    }
+}
